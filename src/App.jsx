0d4e23cbb00ec3ad6a4dfa640da16e3bcb88422a
Chethan M R <mr0chethan@gmail.com>
2021-09-18 15:17:32 +0530

    bored

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+
+function App(){
+    let [activities, setActivities] = useState([]);
+
+    useEffect(()=>{
+        if(activities.length <= 0) return;
+
+        let p = document.createElement("p")
+        p.innerText = "New activity was added";
+        p.classList.add("msg")
+        document.querySelector("body").append(p)
+
+        setTimeout(()=>{
+            p.remove();
+        },2000)
+    },[activities])
+
+    return(
+        <div>
+            <button
+            onClick={async()=>{
+                let res = await fetch("https://www.boredapi.com/api/activity")
+                let json = await res.json()
+                let newActivity = json.activity;
+                setActivities([...activities,newActivity])
+                }
+            }>
+                New Activity
+            </button>
+            <ul>
+                {activities.map((el)=>{
+                    return <li key={el}>{el}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default App
